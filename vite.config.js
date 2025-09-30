04e213f8ff235817f9d8b570eabf93244afc5b02
PedroTsI <ppcantalice121@gmail.com>
2025-09-30 10:06:17 -0300

    Deploy config

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
       // Quando o front-end requisitar /api (ou /classify_file)
       '/classify_file': {
         // Redirecione a requisição para o seu backend
-        target: 'http://127.0.0.1:8000', 
+        target: 'http://127.0.0.1:8000/classify_file', 
         // Necessário para que o backend receba a URL correta
         changeOrigin: true, 
         // Opcional, mas útil se você quiser que o proxy substitua o caminho
